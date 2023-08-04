f3a1eec56d7fe96cec33135a5c5de9af7b3817b8
Luke Lu <lukeupup@gmail.com>
2023-08-04 15:39:00 +0800

    enhance local development mode

--- next.config.js
+++ next.config.js
@@ -3,8 +3,22 @@ const withYaml = require('next-plugin-yaml');
 
 const nextConfig = {
   output: 'export',
-  basePath: '/luke-cv',
+  basePath: '/cv',
   images: { unoptimized: true },
 };
+
+if (process.env.NODE_ENV === 'development') {
+  delete nextConfig.output;
+  nextConfig.redirects = async () => {
+    return [
+      {
+        source: '/',
+        destination: '/cv',
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  };
+}
 
 module.exports = withYaml(nextConfig);
